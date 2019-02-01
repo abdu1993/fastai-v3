f36a7a066f0e0afce63e7f3e7b26e3d50aa2d8c3
abdu1993 <45926965+abdu1993@users.noreply.github.com>
2019-01-31 21:54:23 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1wktxUZ_1fhyBCiGhBU6UoVnut9kdTNNW'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['indian blanket flower', 'orchid flower', 'iris flower', 'daidy flower', 'tulip flower', 'jasmine flower', 'Winecup flower', 'verbana flower', 'dandelion flower', 'bluebonnet flower', 'rose flower', 'sunflower']
 path = Path(__file__).parent
 
 app = Starlette()
